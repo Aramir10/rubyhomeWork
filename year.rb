fdaaf018897856bc879382fc90ec61a9e4aef8e6
Victor Emmanuel <victorgdl89@gmail.com>
2019-07-25 17:34:05 -0500

    Homework 2

--- year.rb
+++ year.rb
@@ -5,7 +5,6 @@
    puts "Age in sec"
     puts (((60) * (24) * 60)* (365) * (29))
 
-
 age = 29
 year = 365
  mercury = 0.241
@@ -14,7 +13,11 @@ year = 365
   jupiter = 11.8
  saturn =29.46
  uranus =  84.01
- neptune =164.8 
+ neptune =164.8
+
+
+ # For example, if I were 21 Earth years old, I would be 21/0.241 = 87 Mercury years old and 21/248.6 = 0.08 Pluto years old.
+
 
 # mercury = ((29 *365)	 / (87.97 ))
 # venus =   ((29 *365)	 / (224.70 ))
